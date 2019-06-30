c7eecea0abc9ebfe7026679b30d49b8a7a10c64a
Wownicehat <34525936+Wownicehat@users.noreply.github.com>
2019-07-01 00:50:46 +0200

    Added cooldown

--- commands.js
+++ commands.js
@@ -6,14 +6,13 @@ const rdmidregex = /R\{(\d*)\}/;
  * @param {*} length
  * @returns result
  */
-function makestring(length)
-{
-	var result           = '';
-	var characters       = 'abcdefghijklmnopqrstuvwxyz';
-	var charactersLength = characters.length;
-	for ( var i = 0; i < length; i++ )
-		result += characters.charAt(Math.floor(Math.random() * charactersLength));
-	return result;
+function makestring(length) {
+    var result = '';
+    var characters = 'abcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++)
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    return result;
 }
 /**
  * Do the requests
@@ -27,23 +26,22 @@ function DoRequests(cmd) {
 
         for (let index = 0; index < cmd.steps.length; index++) {
             var s = cmd.steps[index];
-            
+
             if (curl == undefined) {
                 var preurl = s.url;
                 var e = rdmidregex.exec(preurl);
-                if (e != undefined)
-                {
+                if (e != undefined) {
                     var id = makestring(parseInt(e[1], 10));
                     preurl = preurl.replace(rdmidregex, id);
                 }
                 curl = preurl;
             }
-            var a,b,body = await request({
+            var a, b, body = await request({
                 url: curl,
-                headers:
-                {
+                headers: {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
-                }});
+                }
+            });
             var regex = new RegExp(s.regex);
             var g1 = regex.exec(body)[1]; // get group
             curl = g1;
@@ -57,8 +55,7 @@ module.exports = {
         this.commands = param.commands;
     },
     DoCommand: function (command) {
-        if(command == "help")
-        {
+        if (command == "help") {
             return new Promise((r) => {
                 console.log("Sending help. . .");
                 r(true);
@@ -71,7 +68,7 @@ module.exports = {
             }); // empty promise
 
         return new Promise((resolve) => {
-            var curl = DoRequests(cmd).then((curl) => {
+            DoRequests(cmd).then((curl) => {
                 resolve({
                     url: curl,
                     desc: cmd.description
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 // Start modules / settings
 const client = new Discord.Client();
+const cooldown = new Set();
 const param = JSON.parse(fs.readFileSync('settings.json'));
 const commands = require("./commands.js");
 commands.RegisterCommands(param);
@@ -17,34 +18,41 @@ client.on('ready', () => {
 
 
 client.on('message', msg => {
+
     if (msg.content.startsWith(param.prefix)) {
+        if (cooldown.has(msg.author.id)) {
+            return;
+        }
         var realCommand = msg.content.substring(2, msg.content.length);
+        cooldown.add(msg.author.id);
+        setTimeout(function () {
+            cooldown.delete(msg.author.id);
+        }, 5000)
         commands.DoCommand(realCommand).then((r) => {
             if (r == false)
                 return;
 
-                if(r == true)
-                {
-                    
-                    var helpmsg = `Prefix : \`${param.prefix}\``;
-                    for (const key in param.commands) {
-                        var value = param.commands[key];
-                        var full = key + " - " + value.description;
-                        helpmsg += "\n" + full;
-                    };
-                    msg.channel.send({
-                        embed: {
-                            color: Math.floor(Math.random() * 16777214) + 1,
-                            title: "ImageDiscord",
-                            description: "[Discord](https://discord.gg/b4VG2GX)    [Github](https://github.com/Wownicehat/ImageDiscord-Bot)\n" + helpmsg,
-                            "footer": {
-                                "name": "Bot by John Duckesent",
-                              }
+            if (r == true) {
+
+                var helpmsg = `Prefix : \`${param.prefix}\``;
+                for (const key in param.commands) {
+                    var value = param.commands[key];
+                    var full = key + " - " + value.description;
+                    helpmsg += "\n" + full;
+                };
+                msg.channel.send({
+                    embed: {
+                        color: Math.floor(Math.random() * 16777214) + 1,
+                        title: "ImageDiscord",
+                        description: "[Discord](https://discord.gg/b4VG2GX)    [Github](https://github.com/Wownicehat/ImageDiscord-Bot)\n" + helpmsg,
+                        "footer": {
+                            "name": "Bot by John Duckesent",
                         }
-                    });
-                    
-                    return;
-                }
+                    }
+                });
+
+                return;
+            }
 
 
 
@@ -62,7 +70,7 @@ client.on('message', msg => {
                     },
                     timestamp: new Date()
                 }
-            }).catch((err)=>{
+            }).catch((err) => {
                 msg.channel.send({
                     embed: {
                         color: Math.floor(Math.random() * 16777214) + 1,
@@ -79,8 +87,8 @@ client.on('message', msg => {
                     }
                 });
             });
-        
-        }).catch((err)=>{
+
+        }).catch((err) => {
             msg.channel.send({
                 embed: {
                     color: Math.floor(Math.random() * 16777214) + 1,
@@ -96,8 +104,7 @@ client.on('message', msg => {
     }
 });
 var token = param.token;
-if(token == "ENV")
-{
+if (token == "ENV") {
     console.log("Getting token from env");
     token = process.env.DISCORD_TOKEN;
 }
